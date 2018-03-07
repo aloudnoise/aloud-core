7eb37d64fa8f90c8b62aa73f9c96c2452f4dda3a
aloud <aloudnoise@mail.ru>
2018-03-07 16:59:55 +0600

    1

--- web/views/common/controller_layout.php
+++ web/views/common/controller_layout.php
@@ -1,3 +1,3 @@
-<div class="action-element h-100" id="action_<?=$this->context->id?>_<?=$this->context->action->id?>">
+<div class="action-element" id="action_<?=$this->context->id?>_<?=$this->context->action->id?>">
     <?=$content?>
 </div>
