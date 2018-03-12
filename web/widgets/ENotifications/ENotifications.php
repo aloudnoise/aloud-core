3941f1f5aa0b0cc741b1a36dc28762054307843b
aloud <aloudnoise@mail.ru>
2018-03-12 19:40:41 +0600

    1

--- web/widgets/ENotifications/ENotifications.php
+++ web/widgets/ENotifications/ENotifications.php
@@ -3,6 +3,9 @@
 namespace aloud_core\web\widgets\ENotifications;
 
 use aloud_core\web\components\Widget;
+use aloud_core\web\helpers\ArrayHelper;
+use app\models\Events;
+use common\models\Notifications;
 
 class ENotifications extends Widget
 {
@@ -15,7 +18,32 @@ class ENotifications extends Widget
     public function run()
     {
 
-        return $this->render("templates");
+        $events = Events::getCurrentEvents();
+
+        $notifications = Notifications::find()
+        ->joinWith(['myNotificationStatus'])
+        ->andWhere([
+            'LIKE', 'channels', '___1_'
+        ])
+        ->andWhere([
+            'OR',
+            ['notifications.target_id' => \Yii::$app->user->id, 'target_type' => Notifications::TARGET_USER],
+            ['AND', [
+                'in', 'notifications.target_id', ArrayHelper::map($events, 'id','id'),
+                'target_type' => Notifications::TARGET_EVENT
+            ]],
+            ['target_type' => Notifications::TARGET_ALL],
+            ['target_type' => \Yii::$app->user->can("teacher") ? Notifications::TARGET_TEACHERS : Notifications::TARGET_PUPILS]
+        ])
+            ->andWhere([
+                'IS', 'notification_user_status.id', null
+            ])
+            ->orderBy('notifications.ts DESC')
+            ->all();
+
+        return $this->render("templates", [
+            'notifications' => $notifications
+        ]);
 
     }
 
