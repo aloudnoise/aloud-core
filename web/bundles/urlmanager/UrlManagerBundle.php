a5c35146f28aa3a6df810aedc10fcd602f38baf6
aloud <aloudnoise@mail.ru>
2018-03-14 18:15:07 +0600

    1

--- web/bundles/urlmanager/UrlManagerBundle.php
+++ web/bundles/urlmanager/UrlManagerBundle.php
@@ -21,45 +21,51 @@ class UrlManagerBundle extends AssetBundle
 
     public function registerAssetFiles($view) {
 
-        if (YII_DEBUG || $_GET['force_copy'] == 1) {
-            $this->compileJS();
-        }
         parent::registerAssetFiles($view);
+        $this->compileJS($view);
 
     }
 
 
-    public function compileJS()
+    public function compileJS($view)
     {
-        $urlManager = \Yii::$app->urlManager;
-        $managerVars = get_object_vars($urlManager);
-        $managerVars['urlFormat'] = $urlManager->enablePrettyUrl ? "path" : false;
 
-        $managerVars['rules'] = \Yii::$app->config['components']['urlManager']['rules'];
+        $file = \Yii::getAlias($this->basePath)."/"."Yii.UrlManager.definitions.js";
+        if (!file_exists($file) OR (YII_DEBUG || $_GET['force_copy'] == 1)) {
 
-        if (\Yii::$app->config['components']['urlManager']['rules']) {
-            foreach (\Yii::$app->config['components']['urlManager']['rules'] as $pattern => $route) {
-                //Ignore custom URL classes
-                if(is_array($route) && isset($route['class'])) {
-                    unset($managerVars['rules'][$pattern]);
+            $urlManager = \Yii::$app->urlManager;
+            $managerVars = get_object_vars($urlManager);
+            $managerVars['urlFormat'] = $urlManager->enablePrettyUrl ? "path" : false;
+
+            $managerVars['rules'] = \Yii::$app->config['components']['urlManager']['rules'];
+
+            if (\Yii::$app->config['components']['urlManager']['rules']) {
+                foreach (\Yii::$app->config['components']['urlManager']['rules'] as $pattern => $route) {
+                    //Ignore custom URL classes
+                    if(is_array($route) && isset($route['class'])) {
+                        unset($managerVars['rules'][$pattern]);
+                    }
                 }
             }
-        }
 
-        $encodedVars = Json::encode($managerVars);
+            $encodedVars = Json::encode($managerVars);
 
-        $baseUrl = \Yii::$app->getRequest()->getBaseUrl();
-        $scriptUrl = \Yii::$app->getRequest()->getScriptUrl();
-        $hostInfo = \Yii::$app->getRequest()->getHostInfo();
+            $baseUrl = \Yii::$app->getRequest()->getBaseUrl();
+            $scriptUrl = \Yii::$app->getRequest()->getScriptUrl();
+            $hostInfo = \Yii::$app->getRequest()->getHostInfo();
 
-        $dir = \Yii::getAlias($this->basePath);
-        file_put_contents($dir."/".$this->js[count($this->js)-1], "$(function() {
+            $dir = \Yii::getAlias($this->basePath);
+            file_put_contents($dir."/".$this->js[count($this->js)-1], "$(function() {
                 Yii = Yii || {}; Yii.app = $.extend(Yii.app, {scriptUrl: '{$scriptUrl}',baseUrl: '{$baseUrl}',
                 hostInfo: '{$hostInfo}'});
                 Yii.app.urlManager = new UrlManager({$encodedVars});
                 Yii.app.createUrl = function(route, params, ampersand)  {
                 return this.urlManager.createUrl(route, params, ampersand);};
             });");
+
+        }
+        /* @var $view \aloud_core\web\components\View */
+        $view->registerJsFile($this->baseUrl."/"."Yii.UrlManager.definitions.js",$this->jsOptions);
 
 
     }
--- web/widgets/ENotifications/ENotifications.php
+++ web/widgets/ENotifications/ENotifications.php
@@ -15,34 +15,13 @@ class ENotifications extends Widget
         'ENotifications.js'
     ];
 
+    public $notifications = [];
+
     public function run()
     {
 
-        $events = Events::getCurrentEvents();
-
-        $notifications = Notifications::find()
-        ->joinWith(['myNotificationStatus'])
-        ->andWhere([
-            'LIKE', 'channels', '___1_'
-        ])
-        ->andWhere([
-            'OR',
-            ['notifications.target_id' => \Yii::$app->user->id, 'target_type' => Notifications::TARGET_USER],
-            ['AND', [
-                'in', 'notifications.target_id', ArrayHelper::map($events, 'id','id'),
-                'target_type' => Notifications::TARGET_EVENT
-            ]],
-            ['target_type' => Notifications::TARGET_ALL],
-            ['target_type' => \Yii::$app->user->can("teacher") ? Notifications::TARGET_TEACHERS : Notifications::TARGET_PUPILS]
-        ])
-            ->andWhere([
-                'IS', 'notification_user_status.id', null
-            ])
-            ->orderBy('notifications.ts DESC')
-            ->all();
-
         return $this->render("templates", [
-            'notifications' => $notifications
+            'notifications' => $this->notifications
         ]);
 
     }
