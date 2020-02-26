dac5520d7cdafd5ff5bd90c3fcd9dda48562ecfd
aloudnoise <aloudnoise@mail.ru>
2020-02-27 04:01:31 +0600

    11

--- console/Application.php
+++ console/Application.php
@@ -4,6 +4,13 @@ namespace aloud_core\console;
 class Application extends \yii\console\Application
 {
 
+    public function bootstrap()
+    {
+        \Yii::setAlias("@aloud_core", "@vendor/aloudnoise/aloud-core");
+        parent::bootstrap();
+    }
+
+
     public function coreCommands()
     {
         return array_merge(parent::coreCommands(), [
